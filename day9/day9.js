559634679dfdce1ba0a7d1d731eda073200e68ee
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-16 23:04:15 +0000

    not finished day9 part 1 yet

---
+++ day9/day9.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const actual = fs.readFileSync("./day9/data9.txt", "utf-8");
+
+const data = actual.split("").map(Number);
+
+const unpackedDisk = [];
+
+for (let i = 0; i < data.length; i++) {
+  for (let j = data[i]; j > 0; j--) {
+    if (i % 2 === 0) {
+      unpackedDisk.push(i / 2);
+    } else {
+      unpackedDisk.push(".");
+    }
+  }
+}
+
+unpackedDisk.forEach((block, index) => {
+  if (block === ".") {
+    while (true) {
+      const temp = unpackedDisk.pop();
+      if (temp === ".") {
+        continue;
+      } else {
+        unpackedDisk[index] = temp;
+        break;
+      }
+    }
+  }
+});
+
+let checksum = 0;
+
+unpackedDisk.forEach((block, id) => {
+  checksum += block * id;
+});
+
+console.log(checksum);
