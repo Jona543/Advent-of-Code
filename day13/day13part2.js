20508ec3baa72767d50ad7e3ecc1f27b30b513d7
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-19 22:26:58 +0000

    day13 full

---
+++ day13/day13part2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const actual = fs.readFileSync("day13/data13.txt", "utf-8");
+
+const machines = actual.split("\n\n").map((m) => m.match(/\d+/g).map(Number));
+
+function tokens([aX, aY, bX, bY, pX, pY]) {
+  // aX * a + bX * b = pX
+  // aY * a + bY * b = pY
+  const a = (pX * bY - pY * bX) / (aX * bY - aY * bX);
+  const b = (aX * pY - aY * pX) / (aX * bY - aY * bX);
+  return Number.isInteger(a) && Number.isInteger(b) ? a * 3 + b : 0;
+}
+
+const e = machines.reduce((acc, machine) => {
+  machine[4] = machine[4] + Math.pow(10, 13);
+  machine[5] = machine[5] + Math.pow(10, 13);
+  return acc + tokens(machine);
+}, 0);
+
+console.log(e);
